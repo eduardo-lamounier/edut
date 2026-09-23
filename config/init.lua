a25915e062555381e3da37ce895883e8f42265a8
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-09-23 10:18:57 -0300

    Defines helper function in pre-config for easier setup
    Makes so that `init.lua` can automatically require modules and merge
    the commands definitions. This simplifies the configuration, for example,
    in ./lua/scripting.lua (that now doesn't need to call edut.setup { ... }).
    
    This also fixes a problem: before, if any other module was added, the
    last module to be loaded in init.lua would override the other modules's
    command definitions, now that wouldn't happen because the definitions are
    merged.

--- config/init.lua
+++ config/init.lua
@@ -1,4 +1,25 @@
-require "scripting"
+local function load_commands(modules)
+  local commands = {}
+
+  for _, module_name in ipairs(modules) do
+    local definitions = require(module_name)
+
+    assert(
+      type(definitions) == "table",
+      module_name .. " must return a table of commands"
+    )
+
+    for _, command in ipairs(definitions) do
+      table.insert(commands, command)
+    end
+  end
+
+  return commands
+end
+
+require("edut").setup {
+  commands = load_commands { "scripting" },
+}
 
 -- if you wish to customize something, do it here or in a
 -- new .lua file in lua/ and require it
