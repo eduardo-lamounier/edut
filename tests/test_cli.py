0f0416e6d53a8f7ae9d203383bc23019ccc0739a
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-09-25 22:07:07 -0300

    test: update expected CLI version to 1.0.1

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -97,7 +97,7 @@ class CliTests(unittest.TestCase):
                     self.assertEqual(result.stderr, "")
                     self.assertNotIn("CONFIG_LOADED", result.stdout)
                     if option in ("--version", "-v"):
-                        self.assertEqual(result.stdout, "edut 1.0.0\n")
+                        self.assertEqual(result.stdout, "edut 1.0.1\n")
                     else:
                         self.assertIn("Define and run custom CLI commands", result.stdout)
                         self.assertIn("Usage: edut", result.stdout)
